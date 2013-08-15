080c9a126f5a1ce3158002e9acf9d97e09f03ff8
Alberto Souza <alots.ssa@gmail.com>
2013-08-15 19:07:41 -0300

    updating agent and getting BeanManager in static way

--- src/main/java/br/com/caelum/vraptor/ioc/cdi/BeanManagerSetup.java
+++ src/main/java/br/com/caelum/vraptor/ioc/cdi/BeanManagerSetup.java
@@ -1,6 +1,7 @@
 package br.com.caelum.vraptor.ioc.cdi;
 
 import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -18,13 +19,11 @@ public class BeanManagerSetup implements ServletContextListener {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BeanManagerSetup.class);
 
-	@Inject
-	private BeanManager beanManager;
-
 	public void contextDestroyed(ServletContextEvent event) {
 	}
 
 	public void contextInitialized(ServletContextEvent event) {
+		BeanManager beanManager = CDI.current().getBeanManager();
 		if (event.getServletContext().getAttribute(CDIProvider.BEAN_MANAGER_KEY) == null) {
 			logger.info("Registering BeanManager with key {} in ServletContext",CDIProvider.BEAN_MANAGER_KEY);
 			event.getServletContext().setAttribute(CDIProvider.BEAN_MANAGER_KEY, beanManager);
